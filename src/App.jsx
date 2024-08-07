e9e1dcea51a158d9560c0ac9d250e32078597c5e
Soom <doda2031@naver.com>
2024-08-07 15:40:06 +0900

    리액크 12일차 과제 tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Header from "./components/Header";
 function App() {
   return (
     <main>
-      <Header />
-      <section>
+      <Header/>
+      <section className="flex flex-wrap justify-center gap-5 px-5 py-10">
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
